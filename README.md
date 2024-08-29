69145613ab192f6d663eb4edffb785118bb7b1f5
Antero Oikkonen <antero.oikkonen@gmail.com>
2024-08-29 12:01:44 +0300

    first pactice code, chapter 2

---
+++ README.md
@@ -0,0 +1,5 @@
+# C++ pactices
+
+## Repository contains
+
+This repository contains my solutions of C++ Crash Course: A Fast-Paced introduction book.
---
+++ chapter02/calculator.cpp
@@ -0,0 +1,72 @@
+#include <cstdio>
+#include <cstddef>
+
+enum class Operation {
+  Add,
+  Substract,
+  Multiply,
+  Divide
+};
+
+struct Calculator{
+
+  Operation operation;
+
+  Calculator(Operation operation){
+    this->operation = operation;
+  }
+
+  int calculate(int a, int b){
+
+    int result{0};
+
+    switch (operation)
+    {
+    case Operation::Add: {
+      result = {a + b};
+    } break;
+    case Operation::Substract: {
+      result = {a - b};
+    } break;
+    case Operation::Multiply: {
+      result = {a * b};
+    } break;
+    case Operation::Divide: {
+      result = { a / b};
+    } break;
+      
+  
+    default:
+      printf("Operation not implementet yet.\n");
+      break;
+    }
+    return result;
+  }
+};
+
+int main(){
+
+  Calculator cal = Calculator{Operation::Add};
+
+  int a{1};
+  int b{3};
+
+  int c = cal.calculate(a,b);
+  printf("%d + %d = %d\n",a,b,c);
+
+  cal = Calculator{Operation::Divide};
+
+  c = cal.calculate(a,b);
+  printf("%d / %d = %d\n",a,b,c);
+
+  cal = Calculator{Operation::Substract};
+
+  c = cal.calculate(a,b);
+  printf("%d - %d = %d\n",a,b,c);
+
+  cal = Calculator{Operation::Multiply};
+
+  c = cal.calculate(a,b);
+  printf("%d * %d = %d\n",a,b,c);
+
+}
