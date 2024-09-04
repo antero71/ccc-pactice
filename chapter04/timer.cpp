8301a6532c873863514378533e1c473be3d6ea61
Antero Oikkonen <antero.oikkonen@gmail.com>
2024-09-04 10:46:18 +0300

    comments in English

--- chapter04/timer.cpp
+++ chapter04/timer.cpp
@@ -27,57 +27,57 @@ struct TimerClass {
         std::cout << "Time difference: " << duration.count() << " microseconds" << std::endl;
         std::cout << "Time difference: " << duration.count() / 1000.F << " milliseconds" << std::endl;
         std::cout << "Time difference: " << duration.count() / 1000.F / 1000.0 << " seconds" << std::endl;
-        delete[] name_ptr;  // Varmistetaan oikea muistin vapautus
+        delete[] name_ptr;  // Ensure proper memory deallocation
     }
 
-    // Kopiointikonstruktori
+    // Copy constructor
     TimerClass(const TimerClass& other)
         : max_length{other.max_length}, name_ptr{new char[other.max_length]}, length{other.length} {
         std::strncpy(name_ptr, other.name_ptr, max_length);
-        timestamp = std::chrono::high_resolution_clock::now();  // Uusi timestamp
+        timestamp = std::chrono::high_resolution_clock::now();  // New timestamp
         std::cout << "Copy construct " << name_ptr << std::endl;
     }
 
-    // Kopiointia vastaava sijoitusoperaattori
+    // Copy assignment operator
     TimerClass& operator=(const TimerClass& other) {
         if (this == &other) {
             return *this;
         }
-        delete[] name_ptr;  // Vapautetaan nykyinen resurssi
+        delete[] name_ptr;  // Free the current resource
         name_ptr = new char[other.max_length];
         length = other.length;
         max_length = other.max_length;
         std::strncpy(name_ptr, other.name_ptr, max_length);
-        timestamp = std::chrono::high_resolution_clock::now();  // Uusi timestamp
+        timestamp = std::chrono::high_resolution_clock::now();  // New timestamp
         std::cout << "Copy assign " << name_ptr << std::endl;
         return *this;
     }
 
-    // Siirtokonstruktori
+    // Move constructor
     TimerClass(TimerClass&& other) noexcept
         : timestamp{std::move(other.timestamp)}, name_ptr{other.name_ptr}, length{other.length}, max_length{other.max_length} {
-        other.name_ptr = nullptr;  // Tyhjennetään siirretty olio
+        other.name_ptr = nullptr;   // Clear the moved object
         std::cout << "Move construct" << std::endl;
     }
 
-    // Siirtosijoitusoperaattori
+     // Move assignment operator
     TimerClass& operator=(TimerClass&& other) noexcept {
         if (this == &other) {
             return *this;
         }
-        delete[] name_ptr;  // Vapautetaan nykyinen resurssi
+        delete[] name_ptr;  // Free the current resource
         name_ptr = other.name_ptr;
         length = other.length;
         max_length = other.max_length;
         timestamp = std::move(other.timestamp);
-        other.name_ptr = nullptr;  // Tyhjennetään siirretty olio
+        other.name_ptr = nullptr;  // Clear the moved object
         std::cout << "Move assign" << std::endl;
         return *this;
     }
 
 private:
     std::chrono::time_point<std::chrono::high_resolution_clock> timestamp;
-    char* name_ptr = nullptr;  // Alustetaan nullptriksi
+    char* name_ptr = nullptr;  // Initialize to nullptr
     size_t length{};
     size_t max_length{20};
 };
@@ -85,8 +85,8 @@ private:
 int main() {
     auto timeClass = TimerClass{"first timer"};
     auto timeClass2 = TimerClass("second timer");
-    auto timeClass_copy{timeClass};  // Kopioidaan olio
-    auto timeClass_move{std::move(timeClass)};  // Siirretään olio
+    auto timeClass_copy{timeClass};  // Copy the object
+    auto timeClass_move{std::move(timeClass)};  // Move the object
     auto timeClass_copy2{ timeClass2};
     std::this_thread::sleep_for(std::chrono::seconds(2));
 }
