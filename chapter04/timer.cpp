5b37a1dc165f3f3a4f2c7b46223e398fa2822dff
Antero Oikkonen <antero.oikkonen@gmail.com>
2024-09-04 10:23:31 +0300

    correct string and char * incompability

--- chapter04/timer.cpp
+++ chapter04/timer.cpp
@@ -3,44 +3,55 @@
 #include <ctime>
 #include <cstdio>
 #include <thread> 
+#include <cstring>  // Tarvitaan strlen ja strncpy
 
-struct TimerClass{
+struct TimerClass {
+    TimerClass(const char *name) {  // Muutetaan char* -> const char*
+        timestamp = std::chrono::high_resolution_clock::now();
 
+        const auto name_len = strlen(name);
+        if (name_len + length + 1 > max_length)
+            throw std::runtime_error{"name length should be less than 20"};
 
-TimerClass(char name[]){
-  timestamp = std::chrono::high_resolution_clock::now();
-  if(strlen(name)>19){
-    throw std::runtime_error{"name lenght should be less than 20"};
-  }
-  this.name=name;
-}
+        name_ptr = new char[max_length];
+        std::strncpy(name_ptr, name, max_length - length);
+        length += name_len;
+        name_ptr[length] = 0;
+    }
 
-~TimerClass(){
-  auto end = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - timestamp);
-  std::cout << "Time difference: " << duration.count() << " microseconds" << std::endl;
-  std::cout << "Time difference: " << duration.count()/1000.F << " milliseconds" << std::endl;
-  std::cout << "Time difference: " << duration.count()/1000.F/1000.0 << " seconds" << std::endl;
-}
+    ~TimerClass() {
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - timestamp);
+        std::cout << "Time difference: " << duration.count() << " microseconds" << std::endl;
+        std::cout << "Time difference: " << duration.count() / 1000.F << " milliseconds" << std::endl;
+        std::cout << "Time difference: " << duration.count() / 1000.F / 1000.0 << " seconds" << std::endl;
+        delete[] name_ptr;  // Varmistetaan oikea muistin vapautus
+    }
 
-TimerClass(const TimerClass& other){
- timestamp = other.timestamp;
-}
+    TimerClass(const TimerClass& other)
+        : max_length{other.max_length}, name_ptr{new char[other.max_length]}, length{other.length} {
+        std::strncpy(name_ptr, other.name_ptr, max_length);
+        timestamp = other.timestamp;
+    }
 
-TimerClass& operator=(const TimerClass& other){
-  if ( this == &other){
-    return *this;
-  }
+    TimerClass& operator=(const TimerClass& other) {
+        if (this == &other) {
+            return *this;
+        }
+        const auto new_name = new char[other.max_length];
+        delete[] name_ptr;
+        name_ptr = new_name;
+        length = other.length;
+        max_length = other.max_length;
+        timestamp = other.timestamp;
+        std::strncpy(name_ptr, other.name_ptr, max_length);
+        return *this;
+    }
 
-  timestamp = other.timestamp;
-  return *this;
-}
-
-  // Move-constructor
+    // Move-constructor
     TimerClass(TimerClass&& other) noexcept
-    : timestamp{std::move(other.timestamp)} {
-    
-        other.timestamp = std::chrono::time_point<std::chrono::high_resolution_clock>{}; // Nollataan toisen objektin timestamp
+        : timestamp{std::move(other.timestamp)} {
+        other.timestamp = std::chrono::time_point<std::chrono::high_resolution_clock>{};
     }
 
     // Move-assignment
@@ -49,19 +60,21 @@ TimerClass& operator=(const TimerClass& other){
             return *this;
         }
         timestamp = std::move(other.timestamp);
-        other.timestamp = std::chrono::time_point<std::chrono::high_resolution_clock>{}; // Nollataan toisen objektin timestamp
+        name_ptr = std::move(other.name_ptr);
+        other.timestamp = std::chrono::time_point<std::chrono::high_resolution_clock>{};
+        other.length = 0;
+        other.max_length = 0;
         return *this;
     }
 
 private:
     std::chrono::time_point<std::chrono::high_resolution_clock> timestamp;
-    char name[20];
+    char *name_ptr;  // Korjattu muuttujan nimi
+    size_t length{};
+    size_t max_length{20};
 };
 
-int main(){
-  auto timeClass = new TimerClass{};
-  auto moved_timeClass = std::move(*timeClass);  // Siirretään timeClass -> moved_timeClass
-    
-  std::this_thread::sleep_for(std::chrono::seconds(2));
-  delete timeClass;
+int main() {
+    auto timeClass = TimerClass{"first timer"};
+    std::this_thread::sleep_for(std::chrono::seconds(2));
 }
